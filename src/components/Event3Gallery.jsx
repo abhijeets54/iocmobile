a0df63209e829f9baf85a1df5403f2a10861f196
abhijeets54 <116050986+abhijeets54@users.noreply.github.com>
2024-08-09 15:54:16 +0530

    first commit

--- src/components/Event3Gallery.jsx
+++ src/components/Event3Gallery.jsx
@@ -4,6 +4,18 @@ const Event1Gallery = () => {
   return (
     <div className="text-center py-10 bg-gray-100">
       <h1 className="text-4xl font-bold font-serif mb-8">Youth Fest '23</h1>
+      <br />
+      <p>The Initiators of Change successfully hosted the inaugural "Project Usaari Youth Fest," celebrating young talent
+from Punjab's government schools and NGOs. This event not only showcased diverse skills but also emphasized
+the importance of education and community engagement. * Event Highlights: * Preliminary Rounds:
+Participants showcased their skills in essay writing, declamation, singing, poster making, poetry, Shabad Gyan,
+and creative recycling. Final Showcase: The finale featured outstanding dance, singing, and poetry
+performances, highlighting the rich cultural and artistic heritage of Punjab. Special Moments: Special children
+from the Ashirwad NGO participated, enhancing the event with inclusivity. The highlight was a memorable
+Katha performance by Usaari Evening School students. *Impact and Looking Forward:* The Youth Fest served
+as a platform for empowering the youth, fostering inclusivity, and celebrating diversity. Encouraged by this
+year's success, we plan to expand next year's festivalto include more participants and activities.Join us in
+nurturing the future leaders of Punjab. </p>
       <div className="flex flex-wrap justify-center space-x-4">
         <div className="w-1/4 p-2">
           <img src="/img/28.jpg" alt="Event 1 Photo 1" className="w-full h-full object-contain" />
